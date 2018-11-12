837567dc19c8a4b32476066eb870342d92c2a0cc
MakuZo <MakuZo90@gmail.com>
2018-11-12 18:26:38 +0100

    Fixed requirements.txt and settings.py

--- bloggy/settings.py
+++ bloggy/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'app.apps.AppConfig',
-    'crispy_forms',
     'mptt',
     'rest_framework',
     'rest_framework.authtoken',
